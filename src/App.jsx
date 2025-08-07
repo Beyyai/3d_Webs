419c5f75bd3c4401c2ec83a0148773e8bb964e9b
Beyyai <nadeemnsbh@gmail.com>
2025-08-07 20:26:27 +0500

    refactor: simplify App component by removing unused code and enhancing layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,6 @@
-import { useState } from "react";
-import "./index.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="text-[0px]">Vite + React</h1>
-      <div className="bg-green-500">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className="h-screen flex items-center justify-center"></section>
   );
 }
 
